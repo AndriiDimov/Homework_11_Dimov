f0b884d7c4bbdb5f6132656592cb57837137cc44
Andrii <andrew.dimov77@gmail.com>
2020-11-07 15:53:16 -0500

    Add setInterval

--- main.js
+++ main.js
@@ -4,3 +4,5 @@ var greetingMessage = 'Hello world';
 function greeting(greetingMessage) {
     console.log(greetingMessage + ' ' + this.value);
 }
+
+setInterval(greeting.bind(login, greetingMessage), 2000);
