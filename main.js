50c306e3f6a3ce6a52bbb5c906a8752a57216f05
Andrii <andrew.dimov77@gmail.com>
2020-11-07 15:57:58 -0500

    Add console.log

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ var login = document.querySelector('input[id="name"]');
 var greetingMessage = 'Hello world';
 
 function greeting(greetingMessage) {
-    
+    console.log(greetingMessage + ' ' + this.value);
 }
