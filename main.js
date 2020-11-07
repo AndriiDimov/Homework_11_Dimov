ea0077b7303fab946934a54507e559b1405cd67b
Andrii <andrew.dimov77@gmail.com>
2020-11-07 15:52:24 -0500

    Add function greeting

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 var login = document.querySelector('input[id="name"]');
 var greetingMessage = 'Hello world';
+
+function greeting(greetingMessage) {
+    console.log(greetingMessage + ' ' + this.value);
+}
